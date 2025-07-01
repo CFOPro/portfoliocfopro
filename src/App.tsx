62f137cd45f579c66a1215aac74e58ab837b455d
Gabriel Castillo <gabriel.castillo03@uptc.edu.co>
2025-06-30 21:21:47 -0500

    feat: change the image

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
         <Hero />
         <About />
         <Services />
-        <LeadMagnet />
+        {/* <LeadMagnet /> */}
         <Process />
         <Showcase />
         <Contact />
